5b6db3417d0ac7730fbc4b618e76551a5a75bf2f
cocom8266 <747580295@qq.com>
2025-03-17 11:13:58 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,27 +1,22 @@
 import * as core from "@actions/core";
-import { IncomingWebhook } from "@slack/webhook";
 import axios from "axios";
 import * as qs from "querystring";
 
 (async () => {
   // Validate parameters
-  const [ productId, scheduleId, seatId, webhookUrl ] = [
+  const [productId, scheduleId, seatId] = [
     "product-id",
     "schedule-id",
     "seat-id",
-    "slack-incoming-webhook-url",
   ].map((name) => {
     const value = core.getInput(name);
     if (!value) {
       throw new Error(`melon-ticket-actions: Please set ${name} input parameter`);
     }
-
     return value;
   });
 
   const message = core.getInput("message") ?? "티켓사세요";
-
-  const webhook = new IncomingWebhook(webhookUrl);
 
   const res = await axios({
     method: "POST",
@@ -38,17 +33,15 @@ import * as qs from "querystring";
     }),
   });
 
-  // tslint:disable-next-line
   console.log("Got response: ", res.data);
 
   if (res.data.chkResult) {
     const link = `http://ticket.melon.com/performance/index.htm?${qs.stringify({
       prodId: productId,
     })}`;
-
-    await webhook.send(`${message} ${link}`);
+    console.log(`${message} ${link}`);
   }
 })().catch((e) => {
-  console.error(e.stack); // tslint:disable-line
+  console.error(e.stack);
   core.setFailed(e.message);
 });
